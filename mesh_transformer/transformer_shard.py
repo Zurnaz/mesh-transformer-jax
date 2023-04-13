a5445a52b6c3eab3a629a58b8c45b1ae799e5950
Bogdan Drema <zurnaz@gmail.com>
2023-04-13 16:59:20 +0100

    fix: colab by example

--- mesh_transformer/transformer_shard.py
+++ mesh_transformer/transformer_shard.py
@@ -388,7 +388,7 @@ class CausalTransformer:
                                                              ["batch", ...],
                                                              ["batch", ...],
                                                              ["batch", ...]),
-                                                    out_axes=["batch", ...],
+                                                    out_axes=(["shard", "batch", ...], ["batch", ...]),
                                                     axis_resources={'shard': 'mp', 'batch': 'dp'})
 
         self.train_xmap = jax.experimental.maps.xmap(fun=train,
@@ -407,7 +407,7 @@ class CausalTransformer:
                                                                  ["batch", ...],
                                                                  ["batch", ...],
                                                                  ["shard", ...]),
-                                                        out_axes=["shard", "batch", ...],
+                                                        out_axes=(["shard", "batch", ...], ["batch", ...]),
                                                         axis_resources={'shard': 'mp', 'batch': 'dp'})
 
         self.move_xmap = jax.experimental.maps.xmap(fun=lambda x, _: to_bf16(x),
